fb71d3e1dea31fe999dc6801a820091d8416e92e
vishpat <vishpat@gmail.com>
2017-02-18 13:05:05 -0500

    Found the highest spending customers

---
+++ customer_spending.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+
+
+class MRCustomerSpending(MRJob):
+
+    def steps(self):
+        steps = [
+            MRStep(mapper=self.mapper_get_customer_spending,
+                   reducer=self.reducer_aggregate_customer_spending),
+            MRStep(
+                mapper=self.mapper_total_spending,
+                reducer=self.reducer_total_spending)
+        ]
+        return steps
+
+    def mapper_get_customer_spending(self, _, line):
+        customer, item, amount = line.split(',')
+        yield "%04d" % int(customer), float(amount)
+
+    def reducer_aggregate_customer_spending(self, customer, spendings):
+        yield customer, "%.2f" % sum(spendings)
+
+    def mapper_total_spending(self, customer, spendings):
+        yield spendings, customer
+
+    def reducer_total_spending(self, total_spending, customers):
+        for customer in customers:
+            yield total_spending, customer
+
+
+if __name__ == "__main__":
+    MRCustomerSpending.run()
