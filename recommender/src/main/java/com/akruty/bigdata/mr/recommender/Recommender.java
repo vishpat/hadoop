8e6c6a8e1b1f0ac7984ad1123f4f297787399dc1
vishpat <vishpat@gmail.com>
2017-03-05 20:48:36 -0500

    Remove output directory if it exists

--- recommender/src/main/java/com/akruty/bigdata/mr/recommender/Recommender.java
+++ recommender/src/main/java/com/akruty/bigdata/mr/recommender/Recommender.java
@@ -1,9 +1,9 @@
 package com.akruty.bigdata.mr.recommender;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -16,7 +16,7 @@ import java.util.StringTokenizer;
 
 public class Recommender {
 
-    public static class RecommenderMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class RecommenderMapper extends Mapper<Object, Text, Text, IntWritable> {
         private static final IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
@@ -40,9 +40,15 @@ public class Recommender {
         }
     }
 
-
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+
+
+        FileSystem hdfs = FileSystem.get(conf);
+        Path outputDir = new Path(args[1]);
+        if (hdfs.exists(outputDir)) {
+            hdfs.delete(outputDir, true);
+        }
 
         Job job = Job.getInstance(conf, "Word count");
         job.setJarByClass(Recommender.class);
